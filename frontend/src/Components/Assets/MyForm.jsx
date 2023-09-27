2ea03c0e00ab7a61e6357acb568f12c07b48ea6c
Mishri Lal Sahu <engineermishrilalsahu@gmail.com>
2023-09-27 09:15:30 +0530

    add email js and sweet alert

--- frontend/src/Components/Assets/MyForm.jsx
+++ frontend/src/Components/Assets/MyForm.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const form = [
   {type:'text',place:'Name',nam:'name',class:''},
   {type:'email',place:'Email Address',nam:'email',class:''},
   {type:'text',place:'Subject',nam:'subject',class:''}
 ]
-const MyForm = () => {
+const MyForm = (props) => {
+  const [data,setData] = useState([]);
+  const handleChanges =(event)=>{
+          setData({...data,[event.target.name]: event.target.value});
+  }
   return (
     <>
-      <form className='mt-4'>
+      <form onSubmit={(e)=>{e.preventDefault();props.handleSubmit(data)}} className='mt-4'>
       {
         form.map((el)=>(
-          <input type={el.type} name={el.nam} placeholder={el.place} className={'p-1 px-4 text-[#3d3dd2] bg-[#a3a4aa] w-full rounded-md border border-gray-400 mb-3 outline-none'+el.class} />
+          <input key={el} required onChange={handleChanges} type={el.type} name={el.nam} placeholder={el.place} className={'p-1 px-4 text-[#3d3dd2] bg-[#a3a4aa] w-full rounded-md border border-gray-400 mb-3 outline-none'+el.class} />
         ))
       }
-      <textarea name="message" placeholder='Message..' className='p-1 px-4 text-[#3d3dd2] bg-[#a3a4aa]  w-full rounded-md border border-gray-400 mb-4 outline-none' rows="2"></textarea>
+      <textarea required name="message" onChange={handleChanges} placeholder='Message..' className='p-1 px-4 text-[#3d3dd2] bg-[#a3a4aa]  w-full rounded-md border border-gray-400 mb-4 outline-none' rows="2"></textarea>
       <input type="submit" value="Submit" className='text-white  bg-[#3d3dd2] px-8 text-xl rounded-md cursor-pointer py-1 border-4 border-[#3d3dd2]  hover:text-white' />
       </form>
     </>
